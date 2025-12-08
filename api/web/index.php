34afe936763d6222a1e9f0de7394e020cab3171c
Nugaev <sardor0418@mail.ru>
2025-12-08 07:23:05 +0500

    create curl file sendphoto

--- api/web/index.php
+++ api/web/index.php
@@ -16,12 +16,3 @@ $config = yii\helpers\ArrayHelper::merge(
 );
 
 (new yii\web\Application($config))->run();
-
-
-function vd($var, $exit = true)
-{
-    $dumper = new yii\helpers\BaseVarDumper();
-    echo $dumper::dump($var, 10, true);
-    if ($exit)
-        exit;
-}
