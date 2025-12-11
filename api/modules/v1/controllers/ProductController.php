99924381d54553710321d06212839846e42d1d1b
M A C W I N <mavzurovnurali@gmail.com>
2025-12-11 21:03:39 +0500

    cors added to api

--- api/modules/v1/controllers/ProductController.php
+++ api/modules/v1/controllers/ProductController.php
@@ -23,7 +23,7 @@ class ProductController extends ActiveController
             'class' => Cors::class,
 
             'cors' => [
-                'Origin' => ["*"],
+                'Origin' => ["https://shop.nugaev.uz/admin/site/index"],
                 'Access-Control-Request-Method' => ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
                 'Access-Control-Allow-Credentials' => true,
                 'Access-Control-Allow-Headers' => ['Content-Type', 'Authorization'],
